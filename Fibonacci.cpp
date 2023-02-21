178591e326b586854089c5b73be36a1bd73239b1
Iuvennis <102306927+Iuvennis@users.noreply.github.com>
2023-02-21 13:30:01 +0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -8,13 +8,13 @@ int main()
     int a2 = 1;
     int n;
 
-    cout << "Enter the n, motherfucker: ";
+    cout << "Enter the n, dude: ";
     cin >> n;
-    cout << endl << "Get it back, bitch: " << endl;
+    cout << endl << "Get it back, bro: " << endl;
 
     switch (n) {
     case(0):
-        cout << "Fuck you. Am I a joke for you?";
+        cout << "Am I a joke for you?";
     default:
         for (int i = 0; i < n; i++) {
             cout << a1 << endl;
