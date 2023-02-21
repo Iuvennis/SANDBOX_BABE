e35bd7b8e616dafa24d2b219c166a4e3792bc03f
Iuvennis <102306927+Iuvennis@users.noreply.github.com>
2023-02-21 12:51:20 +0300

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    int a1 = 0;
+    int a2 = 1;
+    int n;
+
+    cout << "Enter the n, motherfucker: ";
+    cin >> n;
+    cout << endl << "Get it back, bitch: " << endl;
+
+    switch (n) {
+    case(0):
+        cout << "Fuck you. Am I a joke for you?";
+    default:
+        for (int i = 0; i < n; i++) {
+            cout << a1 << endl;
+            a1 = a1 + a2;
+            a2 = a1 - a2;    
+        }
+
+    }
+}
